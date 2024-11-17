f097340ed440a4fbfeda708232d9540f30699154
avindhawale <avin.dhawale@gmail.com>
2024-11-17 17:20:09 +0530

    @Input @Output and Event Emitters

---
+++ src/app/components/course-card/course-card.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Course } from "src/app/model/course";
+
+@Component({
+  selector: "course-card",
+  standalone: true,
+  imports: [],
+  templateUrl: "./course-card.component.html",
+  styleUrl: "./course-card.component.css",
+})
+export class CourseCardComponent {
+  @Input()
+  course: Course;
+
+  @Output()
+  selectedCourse = new EventEmitter<Course>();
+
+  courseClicked(): void {
+    this.selectedCourse.emit(this.course);
+  }
+}
