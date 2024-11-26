52edf25ee80ab536bdfc49fe02741ba523472939
avindhawale <avin.dhawale@gmail.com>
2024-11-26 19:22:22 +0530

    Angular 18 : @defer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { CourseCardComponent } from "./components/course-card/course-card.component";
+import { DeferComponent } from "./components/defer/defer.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, CourseCardComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    CourseCardComponent,
+    DeferComponent,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
